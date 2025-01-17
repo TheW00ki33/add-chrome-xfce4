65e860d9429c48aa929a3eb01362785e1a5e8753
Kevin <167295152+TheW00ki33@users.noreply.github.com>
2025-01-17 11:42:12 +0100

    Update thechromexfce.sh
    added install params

--- thechromexfce.sh
+++ thechromexfce.sh
@@ -1,22 +1,33 @@
 
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 
-read -p "PRESS ENTER TO CONTINUE...(1/5)"
+read -p "PRESS ENTER TO CONTINUE...(1/8)"
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
-read -p "PRESS ENTER TO CONTINUE...(2/5)"
+read -p "PRESS ENTER TO CONTINUE...(2/8)"
+
+sudo apt install ./chrome-remote-desktop_current_amd64.deb
+
+read -p "PRESS ENTER TO CONTINUE...(3/8)"
+
+sudo apt install ./google-chrome-stable_current_amd64.deb
+
+read -p "PRESS ENTER TO CONTINUE...(4/8)"
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+read -p "PRESS ENTER TO CONTINUE...(5/8)"
 
 sudo DEBIAN_FRONTEND=noninteractive \
 	apt install --assume-yes xfce4 desktop-base dbus-x11 xscreensaver
 
-read -p "PRESS ENTER TO CONTINUE...(3/5)"
+read -p "PRESS ENTER TO CONTINUE...(6/8)"
 
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
 
-read -p "PRESS ENTER TO CONTINUE...(4/5)"
+read -p "PRESS ENTER TO CONTINUE...(7/8)"
 
 sudo systemctl disable lightdm.service
 
-read -p "FINISCH! --> PRESS ENTER TO CLOSE...(5/5)"
-
+read -p "FINISCH! --> PRESS ENTER TO CLOSE...(8/8)"
